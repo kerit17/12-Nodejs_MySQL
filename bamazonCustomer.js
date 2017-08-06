c63c716a44568e4f5747aa54ab3c7937301de8a1
Keri Schreckengost <keris@live.com>
2017-08-06 17:44:01 -0600

    Inquirer #2

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,26 +12,55 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err){
 	if(err) throw err;
-	console.log("connected as id " + connection.threadID + "\n");
 	display();
 });
 
 //function to display items for sale: ids, names, and prices
 function display(){
 	connection.query("SELECT * FROM products", function(err, res){
-		if (err) throw err;
+			if (err) throw err;
 			console.table(res);
-		choice();
-	})
-};
-	
-//function to ask user is going to purchase
-function choice(){
- 	inquirer
- 	.prompt([
- 	{
- 		name: "choice",
- 		type: "input",
- 		message: "What is the ID of the product you would like to purchase?"
- 	}]);
-}
+
+		inquirer
+ 			.prompt([
+ 				{
+		 			name: "choice",
+		       		type: "input",
+		        	message: "What is the ID of the product you would like to purchase?"
+        		},
+        		{
+					name: "number",
+		       		type: "input",
+		        	message: "How many items would you like to puchase?"
+        		}
+        	])
+        	.then(function(answer){
+        		var chosenItem;
+        		for (var i=0; i < res.length; i++){
+        				if(res[i].item_id === answer.choice){chosenItem = res[i];
+        				}
+        			}
+        		//determine if quantity available
+        		if(chosenItem.stock_quantity < parseInt(answer.number)){
+        			console.log("Item(s) is/are available")//update database
+        			connection.query("UPDATE products SET ? WHERE ?",[
+        				{
+        					stock_quantity: number
+        				},
+        				{
+        					item_id: chosenItem
+        				}],
+        				function(error){
+        					if (error) throw err;
+        					console.log("Quantity puchased!");
+        					display();
+        				}
+        			);	
+        			}
+        		else{
+        			console.log("Insufficient Quantity! Please make another selection.");
+        			display();
+        		}
+        	//closes the .then		
+        		});
+    })};
