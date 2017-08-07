e95d49ac630e8cef175426c03939b9b8f58e22c9
Keri Schreckengost <keris@live.com>
2017-08-06 18:18:08 -0600

    updated

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,49 +18,55 @@ connection.connect(function(err){
 //function to display items for sale: ids, names, and prices
 function display(){
 	connection.query("SELECT * FROM products", function(err, res){
-			if (err) throw err;
-			console.table(res);
+		if (err) throw err;
+		console.table(res);
 
+//order
 		inquirer
- 			.prompt([
- 				{
-		 			name: "choice",
-		       		type: "input",
-		        	message: "What is the ID of the product you would like to purchase?"
-        		},
-        		{
-					name: "number",
-		       		type: "input",
-		        	message: "How many items would you like to puchase?"
-        		}
-        	])
-        	.then(function(answer){
-        		var chosenItem;
-        		for (var i=0; i < res.length; i++){
-        				if(res[i].item_id === answer.choice){chosenItem = res[i];
-        				}
-        			}
-        		//determine if quantity available
-        		if(chosenItem.stock_quantity < parseInt(answer.number)){
-        			console.log("Item(s) is/are available")//update database
-        			connection.query("UPDATE products SET ? WHERE ?",[
-        				{
-        					stock_quantity: number
-        				},
-        				{
-        					item_id: chosenItem
-        				}],
-        				function(error){
-        					if (error) throw err;
-        					console.log("Quantity puchased!");
-        					display();
-        				}
-        			);	
-        			}
-        		else{
-        			console.log("Insufficient Quantity! Please make another selection.");
-        			display();
-        		}
-        	//closes the .then		
-        		});
-    })};
+			.prompt([
+			{
+	 			name: "choice",
+	       		type: "input",
+	        	      message: "What is the ID of the product you would like to purchase?"
+       		},
+       		{
+				name: "number",
+	       		type: "input",
+	        	      message: "How many items would you like to puchase?"
+       		}
+       	])
+
+//compare if quantity is in stock
+       	.then(function(answer){
+       		var chosenItem;
+           		for (var i=0; i < res.length; i++){
+       				if(res[i].item_id === answer.choice){chosenItem = res[i];
+       				}
+       			}
+
+       		if(chosenItem.stock_quantity < parseInt(answer.number)){
+       			console.log("Item(s) is/are available")
+
+//update database
+       			connection.query("UPDATE products SET ? WHERE ?",[
+       				{
+       					stock_quantity: number
+       				},
+       				{
+       					item_id: chosenItem
+       				}],
+       				function(error){
+       					if (error) throw err;
+       					console.log("Quantity puchased!");
+       					display();
+       				}
+       			);	
+       			}
+//if quanitity is not available        		
+       		else{
+       			console.log("Insufficient Quantity! Please make another selection.");
+       			display();
+       		}
+       	// closes the .then		
+       		});
+})};
